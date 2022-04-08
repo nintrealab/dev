e40985faa4459fa0bb13eda79fc3188b5e881af8
LEAT SOPHAT <sophat.leat@TURBOTECH.COM>
2022-04-08 14:11:53 +0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,34 +1,9 @@
-const withTM    = require('next-transpile-modules')(['three'])
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports  = withTM()
-module.exports  = {
-    assetPrefix: isProd ? '/your-github-repo-name/' : '',
-    images: {
-        domains: [
-            '2.bp.blogspot.com',
-            's.gravatar.com',
-            'www.gfxkh.com',
-            'telegra.ph',
-            'static01.nyt.com',
-            'cdn.filestackcontent.com',
-            'i.mydramalist.com',
-            '855-files.netlify.app',
-            'nintrea.org',
-            'nintrea.live',
-            'nintrea.xyz',
-            'mir-s3-cdn-cf.behance.net',
-            'media-exp1.licdn.com',
-            'www.nintrea.org',
-            'v1.nintrea.org',
-            'unsplash.com',
-            'images.unsplash.com',
-            'novel.nintrea.org',
-            'file.nintrea.org',
-            'bedok.nintrea.org',
-            'bundit.nintrea.org',
-            'sophat.nintrea.org',
-            'nintrea.netlify.app',
-        ],
-    },
-}   
+module.exports = {
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-import '../styles/globals.css';
-import 'prismjs/themes/prism-tomorrow.css';
-import Script from 'next/script';
-import '../public/css/nintrea@icons.css';
-function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Script
-                id='ga-script'
-                strategy='lazyOnload'
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+import "../styles/globals.css";
 
-            <Script
-                id='data-ga'
-                strategy='lazyOnload'>
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                  
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-                `}
-            </Script>
-            <span className="theme-bejamas" />
-            <Component {...pageProps} />
-        </>
-    );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
